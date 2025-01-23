05d277c3bae3babce230af45fbd01b6845d7961f
Akarkab-Maryam <ourzazat.ne1998@gmail.com>
2025-01-23 11:00:18 +0100

    Add files via upload

---
+++ visualiserProduit.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 23 09:58:49 2025
+
+@author: maryam
+"""
+
+import pandas as pd
+
+# Charger le fichier CSV
+df = pd.read_csv('C:\\Users\\maryam\\Downloads\\produits.csv')
+
+
+# Afficher les premières lignes du fichier
+print(df.head(40))
